74d7e83453bd902f1ec9c817f0038e8a7e2cc51e
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-04-13 15:45:32 -0600

    Ejercicio3

---
+++ Curso/Program.cs
@@ -0,0 +1,24 @@
+﻿int curso, error=0; 
+do
+{
+    Console.WriteLine("Ingrese el número de estudiantes en el curso:");
+    if(int.TryParse(Console.ReadLine(), out curso))
+    {
+        if(curso<1)
+        {
+            Console.WriteLine("El número de estudiantes no es válido. Por favor, ingrese un número válido."); error++; continue;
+        }
+        else if(curso<=40)
+        {
+            Console.WriteLine("El número de estudiantes en el curso es: " + curso);
+        }  
+        else if(curso>40)
+        {
+            Console.WriteLine("El número de estudiantes excede el límite permitido."); error++; continue;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número."); error++; continue;
+    }
+} while (error!=0);
