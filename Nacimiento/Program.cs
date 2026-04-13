48a9af60e2854d92eed8734c8f9945dfd64419a6
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-04-13 16:10:58 -0600

    Ejercicio4.2

--- Nacimiento/Program.cs
+++ Nacimiento/Program.cs
@@ -5,5 +5,19 @@ do
     Console.WriteLine("Ingrese su año de nacimiento (dd/mm/yyyy):");
     DateTime año;
     if(DateTime.TryParse(Console.ReadLine(), out año))
-    
+    {
+        if (año.Year < 1900 || año.Year > DateTime.Now.Year)
+        {
+            Console.WriteLine("El año de nacimiento no es válido. Por favor, ingrese un año válido."); error++; continue;
+        }
+        else
+        {
+            int edad = DateTime.Now.Year - año.Year;
+            Console.WriteLine("Su edad es: " + edad);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese una fecha en el formato dd/mm/yyyy."); error++; continue;
+    }
 } while (true);
