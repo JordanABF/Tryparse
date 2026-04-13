4196126706507ce627d91ac8ebf1755ccb7870ef
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-04-13 16:08:25 -0600

    ejercicio4

---
+++ Nacimiento/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+int error = 0;
+do
+{
+    Console.WriteLine("Ingrese su año de nacimiento (dd/mm/yyyy):");
+    DateTime año;
+    if(DateTime.TryParse(Console.ReadLine(), out año))
+    
+} while (true);
