ed9cf665e2cddcd683352054063118d455a594a8
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-04-13 15:13:22 -0600

    Agregar archivos de proyecto.

---
+++ Edad/Program.cs
@@ -0,0 +1,26 @@
+﻿int edad, error = 0;
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su edad:");
+    
+    if(int.TryParse(Console.ReadLine(), out edad))
+    {
+        if (edad < 0 )
+        {
+            Console.WriteLine("La edad es invalida"); error = 1;
+        }
+        else if(edad <= 99)
+        {
+            Console.WriteLine("Su edad es de: "+edad);
+        }
+        else if (edad > 99)
+        {
+            Console.WriteLine("La edad excede lo razonable");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error. Por favor, ingrese un número.");
+    }
+} while (error!=0);
