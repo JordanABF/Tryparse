8d9a5c4d075bd7c50b38eed87c951a803ece926f
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-04-13 15:23:56 -0600

    ejercicio2

---
+++ Precio/Program.cs
@@ -0,0 +1,13 @@
+﻿int error=0;
+
+do
+ {
+    Console.WriteLine("Ingrese el precio del producto:");
+    decimal precio;
+    if(decimal.TryParse(Console.ReadLine(), out precio))
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número.");
+        error++;
+        continue;
+    }
+} while (error!=0);
